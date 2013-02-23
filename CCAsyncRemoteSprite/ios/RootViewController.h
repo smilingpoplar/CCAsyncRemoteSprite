8e7197a09bb3877112771e2376f7e600ad764207
smilingpoplar <smilingpoplar@gmail.com>
2013-02-23 23:31:40 +0800

    Initial Commit

---
+++ CCAsyncRemoteSprite/ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  CCAsyncRemoteSpriteAppController.h
+//  CCAsyncRemoteSprite
+//
+//  Created by smilingpoplar on 13-2-23.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
